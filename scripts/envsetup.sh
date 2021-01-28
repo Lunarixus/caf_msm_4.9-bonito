d864bc375619d780f42e572690a81dac93e439df
Lunarixus <Nathan@lunarixus.dev>
2021-01-28 17:26:16 +0000

    scripts: Add my personal env setup script
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

---
+++ scripts/envsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Simple clang envsetup script
+export CROSS_COMPILE_ARM32=/run/media/lunarixus/07a4e4f7-f758-4a3e-8f30-93d19591469b/home/lunarixus/hentaiOS/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export PATH="/run/media/lunarixus/07a4e4f7-f758-4a3e-8f30-93d19591469b/home/lunarixus/hentaiOS/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:/run/media/lunarixus/07a4e4f7-f758-4a3e-8f30-93d19591469b/home/lunarixus/hentaiOS/prebuilts/clang/host/linux-x86/clang-r383902/bin:$PATH"
+echo "Clang paths should now be set up"
